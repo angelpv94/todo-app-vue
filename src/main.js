08e5716edf5fb6c5bd91463e9e125e050a44c936
Angel Perez <aperez@typeiqs.com>
2019-08-05 12:46:00 -0400

    button created

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue';
 import '@/assets/css/tailwind.css';
 
 import Card from './components/Card.vue';
+import Button from './components/Button.vue';
+
 
 Vue.config.productionTip = false;
 
 Vue.component('todo-card', Card);
+Vue.component('todo-button', Button);
 
 new Vue({
   render: h => h(App),
