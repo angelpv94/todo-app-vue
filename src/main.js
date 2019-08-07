58d5afb68791a79ba6a53e7a21da6f4476a774ad
Angel Perez <aperez@typeiqs.com>
2019-08-07 13:51:33 -0400

    header component imported

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import '@/assets/css/tailwind.css';
 import Card from './components/Card.vue';
 import Button from './components/Button.vue';
 import Input from './components/Input.vue';
+import Header from './components/Header.vue';
 
 
 Vue.config.productionTip = false;
 
 Vue.component('todo-card', Card);
 Vue.component('todo-button', Button);
-Vue.component('todo-input', Input)
+Vue.component('todo-input', Input);
+Vue.component('todo-header', Header);
 
 new Vue({
   render: h => h(App),
