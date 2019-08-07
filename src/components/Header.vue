50504171faa0a803f7cc7cece313518904b4b6cf
Angel Perez <aperez@typeiqs.com>
2019-08-07 13:41:03 -0400

    header created

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="header row center-align">
+        <div class="col s12">
+          <h1 :class="[cardHeader]" class="text-gray-600 text-4xl font-semibold">{{ cardHeader }}</h1>
+        </div>
+      </header>
+</template>
+
+
+<script>
+export default {
+    props: {
+        cardHeader: {
+            default: 'Título',
+            type: String
+        }
+    },
+
+    data () {
+        return {
+
+        };
+    },
+
+    mounted () {
+
+    },
+
+    methods: {
+
+    }
+}
+</script>
