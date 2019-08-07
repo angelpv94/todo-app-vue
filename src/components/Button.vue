af78d77b8b78c30fe601a4f4017710ba3c84726a
Angel Perez <aperez@typeiqs.com>
2019-08-07 13:38:19 -0400

    button is customizable

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,24 @@
 <template>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
+    <button :class="[buttonBg, hoverColor, textColor]" class="font-bold py-2 px-4 rounded">
+        <slot></slot>
     </button>
 </template>
 
 <script>
 export default {
     props: {
-
+        buttonBg: {
+            default: 'bg-blue-500',
+            type: String
+        },
+        hoverColor: {
+            default: 'hover:bg-blue-700',
+            type: String
+        },
+        textColor: {
+            default: 'text-white',
+            type: String
+        }
     },
 
     data () {
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
-        <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
+        
+        <todo-header cardHeader="Esto es un título de prueba"></todo-header>
 
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
             This is a test
         </todo-card>
 
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
-            <todo-button></todo-button>
-        </todo-card>
+            <todo-button>Botón</todo-button>
+         </todo-card>
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
             <todo-input></todo-input>
         </todo-card>
